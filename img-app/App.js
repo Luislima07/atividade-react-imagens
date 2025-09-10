e344ac44cface910eb0cab612214e9b8043fca79
Luislima07 <161834927+Luislima07@users.noreply.github.com>
2025-09-10 14:59:51 -0300

    Layout atualizado

--- img-app/App.js
+++ img-app/App.js
@@ -1,93 +1,108 @@
-import React, { use, useState } from 'react'
-import { StyleSheet, Text, TextInput, Button, Image, View, TouchableOpacity } from 'react-native';
-
+import React, { useState } from 'react';
+import { StyleSheet, Text, Image, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   const [images1, setImages1] = useState(require('./src/img1.jpg'));
-  const [images2, setImages2] = useState(require('./src/img3.jpg'))
-  const [images3, setImages3] = useState(require('./src/img5.jpg'))
+  const [images2, setImages2] = useState(require('./src/img3.jpg'));
+  const [images3, setImages3] = useState(require('./src/img5.jpg'));
+  const [images4, setImages4] = useState(require('./src/img7.jpg'));
 
   function mudarImagem1() {
-    if (images1 === require('./src/img1.jpg')) {
-      setImages1(require('./src/img2.jpg'));
-    } else {
-      setImages1(require('./src/img1.jpg'));
-    }
+    setImages1(images1 === require('./src/img1.jpg') ? require('./src/img2.jpg') : require('./src/img1.jpg'));
   }
 
   function mudarImagem2() {
-    if (images2 === require('./src/img3.jpg')) {
-      setImages2(require('./src/img4.jpg'));
-    } else {
-      setImages2(require('./src/img3.jpg'));
-    }
+    setImages2(images2 === require('./src/img3.jpg') ? require('./src/img4.jpg') : require('./src/img3.jpg'));
   }
 
   function mudarImagem3() {
-    if (images3 == require('./src/img5.jpg')) {
-      setImages3(require('./src/img6.jpg'))
-    } else {
-      setImages3(require('./src/img5.jpg'))
-    }
-    };
-    return (
-      <View style={styles.principal}>
-        <Text style={styles.texto}>Troca Imagem!</Text>
-        <View style={styles.container1}>
-          <Image source={images1} style={styles.img} />
-          <TouchableOpacity>
-            <Button title='Pressione aqui' onPress={mudarImagem1} />
-          </TouchableOpacity>
-        </View>
-          <View style={styles.container2}>
-            <Image style={styles.img} source={images2} />
-            <TouchableOpacity>
-              <Button title='Pressione aqui' onPress={mudarImagem2} />
-            </TouchableOpacity>
-          </View>
-          <View style={styles.container3}>
-            <Image style={styles.img} source={images3} />
-            <TouchableOpacity>
-              <Button title='Pressione aqui' onPress={mudarImagem3} />
-            </TouchableOpacity>
-          </View>
-      </View>
-    );
+    setImages3(images3 === require('./src/img5.jpg') ? require('./src/img6.jpg') : require('./src/img5.jpg'));
   }
 
-  //Styles
-  const styles = StyleSheet.create({
-    principal: {
-      flex: 1,
-      backgroundColor: '#5e5e5eff',
-    },
-    container1: {
-      flex: 2,
-      alignItems: 'center',
-    },
-    container2: {
-      flex: 2,
-      alignItems: 'center',
-      backgroundColor: '#6a0002ff',
-    },
-    container3: {
-      flex: 3,
-      alignItems: 'center',
-      backgroundColor: '#133238',
-    },
-    texto: {
-      fontSize: 20,
-      fontWeight: 'bold',
-      padding: 20,
-      color: '#ffff',
-      textAlign: 'center',
-      backgroundColor: '#323030ff'
-    },
-    img: {
-      width: 200,
-      height: 200,
-      borderColor: '#ffffff',
-      borderRadius: 50,
-      margin: 20,
-    },
-  })
+  function mudarImagem4() {
+    setImages4(images4 === require('./src/img7.jpg') ? require('./src/img8.webp') : require('./src/img7.jpg'));
+  }
+
+  return (
+    <View style={styles.principal}>
+      <Text style={styles.texto}>Troca Imagem!</Text>
+      <View style={styles.grid}>
+        {/* Linha 1 */}
+        <View style={styles.box}>
+          <Image source={images1} style={styles.img} />
+          <TouchableOpacity style={styles.btn} onPress={mudarImagem1}>
+            <Text style={styles.btnTexto}>Trocar Imagem</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.box}>
+          <Image source={images2} style={styles.img} />
+          <TouchableOpacity style={styles.btn} onPress={mudarImagem2}>
+            <Text style={styles.btnTexto}>Trocar Imagem</Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* Linha 2 */}
+        <View style={styles.box}>
+          <Image source={images3} style={styles.img} />
+          <TouchableOpacity style={styles.btn} onPress={mudarImagem3}>
+            <Text style={styles.btnTexto}>Trocar Imagem</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.box}>
+          <Image source={images4} style={styles.img} />
+          <TouchableOpacity style={styles.btn} onPress={mudarImagem4}>
+            <Text style={styles.btnTexto}>Trocar Imagem</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+//Styles
+const styles = StyleSheet.create({
+  principal: {
+    flex: 1,
+    backgroundColor: '#5e5e5e',
+  },
+  texto: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    padding: 20,
+    color: '#fff',
+    textAlign: 'center',
+    backgroundColor: '#323030',
+  },
+  grid: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap', // permite quebrar linha
+  },
+  box: {
+    width: '50%', // 2 por linha
+    height: '50%', // 2 linhas
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: '#000',
+  },
+  img: {
+    width: 120,
+    height: 120,
+    borderRadius: 15,
+    marginBottom: 10,
+  },
+  btn: {
+    backgroundColor: '#9f9f9f',
+    padding: 10,
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: '#000',
+  },
+  btnTexto: {
+    fontWeight: 'bold',
+    color: '#d10000',
+  },
+});
